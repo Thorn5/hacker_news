c98efcbc030d7c25e0161353b1a0ef3bee736515
shima1364 <sh.movafagh.474@gmail.com>
2023-01-21 12:33:40 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React,  {useEffect , useState} from 'react';
 import './App.css';
 
 function App() {
+ const [news , setNews] = useState([]);
+
+  useEffect(()=> {
+    getNews();
+  },[])
+  const getNews = async () => {
+    const response = await fetch('http://hn.algolia.com/api/v1/search_by_date?');
+    const data = await response.json();
+    setNews(data.hits);
+    console.log(data.hits);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input className="search-bar" type= "text"/>
+        <button className="search-button" type="submit">search</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ const News = () => {
+    return (
+        <div>
+            <h1>Title</h1>
+            <p>body</p>
+            <img src='' alt="news"/>
+
+        </div>
+    );
+ };
+
+ export default News;

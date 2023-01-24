147a9ed07f8b9e698fb2a4fd3d7779ce597423ea
shima1364 <sh.movafagh.474@gmail.com>
2023-01-24 02:03:41 +0100

    9

--- src/App.js
+++ src/App.js
@@ -1,46 +1,52 @@
 import React,  {useEffect , useState} from 'react';
 import News from './components/News';
 import './App.css';
+import LoadingSpinner from './components/LoadingSpinner';
 
 function App() {
  const [newses , setNewses] = useState([]);
  const [search , setSearch] = useState("");
  const [query , setQuery] = useState("bitcoin");
+ const [loading , setLoading] = useState(true);
 
   useEffect(()=> {
     getNews();
-  },[])
+  },[query])
   const getNews = async () => {
     const response = await fetch(`http://hn.algolia.com/api/v1/search?query=${query}`);
     const data = await response.json();
     setNewses(data.hits);
-    console.log(data.hits);
+    setLoading(false);
   }
   const updateSearch = e => {
     setSearch(e.target.value);
   }
   const getSearch = e => {
     e.preventDefault();
-    setQuery (search);
+    setQuery(search);
+    setSearch("");
   }
   return (
     <div className="App">
       <form className='search-form' onSubmit={getSearch}>
-        <input className="search-bar" type= "text" value={search}/>
-        <button  className="search-button" 
-        type="submit"
-        onClick={updateSearch}
+        <input className="search-bar"
+        type= "text"
+        value={search}
+        onChange={updateSearch} 
+        />
+        <button className="search-button" type="submit"
         >search</button>
       </form>
-      {newses.map(news => (
+     {loading === true ? (<LoadingSpinner/>) :
+      (newses.map(news => (
         <News 
           key={news.story_id}
-          title = {news.title}
-          url = {news.url}
-          author= {news.author}
+          title={news.story_title}
+          url={news.url}
+          author={news.author}
           // date = {new Intl.DateTimeFormat('en-Us' ,{year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(items.created_at_i)}
           />
-      ))};
+      )))};
     </div>
   );
 }

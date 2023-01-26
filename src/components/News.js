184ff7674be0a65bf46fbd40aa31de8c9821949f
shima1364 <sh.movafagh.474@gmail.com>
2023-01-26 03:05:51 +0100

    6

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,11 @@
 import React from 'react';
- const News = ({title , url, author }) => {
+ const News = ({title , url, author}) => {
     return (
         <div>
             <h3>{title}</h3>
             <a target="_blank" href={url}>{url}</a>
             <p>{author}</p>
+            <div class="ui divider"></div>
         </div>
     );
  };

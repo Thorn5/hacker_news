49375158800b0afb13bd1a870c879aac42a4e6c4
Trygve Horn <trygvehorn@gmail.com>
2023-01-28 16:26:09 +0100

    snapshot

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
- const Home = () => {
+const Home = () => {
     return (
         <div>
-            Home
+            <h1>Welcome to Hacker News</h1>
+            <p>This group project meets the objectives presented in the&nbsp;
+                <a href="https://platform.wbscodingschool.com/courses/full-stack-web-app/12751/" target="_blank">WBS ⭐️ Hacker News</a> project.</p>
+            <ol><h3>Project Features</h3>
+                <li>Searchable news page</li>
+                <li>page navigation within searches</li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ol>
+
+
         </div>
     )
- }
+}
 
- export default Home;
+export default Home;

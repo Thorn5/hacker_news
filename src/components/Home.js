a413b09e9c4b3c7674e6d9c777431171f2f54e5c
Trygve Horn <trygvehorn@gmail.com>
2023-01-28 16:54:57 +0100

    features

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,11 @@ const Home = () => {
             <p>This group project meets the objectives presented in the&nbsp;
                 <a href="https://platform.wbscodingschool.com/courses/full-stack-web-app/12751/" target="_blank">WBS ⭐️ Hacker News</a> project.</p>
             <ol><h3>Project Features</h3>
-                <li>Searchable news page</li>
+                <li>searchable news page</li>
                 <li>page navigation within searches</li>
-                <li></li>
+                <li>progress bar</li>
+                <li>pagification / page navigation</li>
+                <li>Library based CSS styling</li>
             </ol>
         </div>
     )

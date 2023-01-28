be36fe8c5d12ee073f6c104e75824ef0fa9203d1
Trygve Horn <trygvehorn@gmail.com>
2023-01-28 13:42:05 +0100

    got basic tabs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,48 @@ import FixedQuery from './views/FixedQuery';
 import DynamicQuery from './views/DynamicQuery';
 import NoPage from './views/NoPage';
 
+// tabs 
+import 'react-dyn-tabs/style/react-dyn-tabs.css';
+import 'react-dyn-tabs/themes/react-dyn-tabs-card.css';
+import useDynTabs from 'react-dyn-tabs';
+
 function App() {
+
+  //tabs
+  const options = {
+    tabs: [
+      {
+        id: '1',
+        title: 'tab 1',
+        panelComponent: (props) => <p> panel 1 </p>,
+      },
+      {
+        id: '2',
+        title: 'tab 2',
+        panelComponent: (props) => <p> panel 2 </p>,
+      },
+    ],
+    selectedTabID: '1',
+  };
+
+  let _instance;
+  const [TabList, PanelList, ready] = useDynTabs(options);
+  ready((instance) => {
+    _instance = instance;
+  });
+  const addTab3 = function () {
+    // open tab 3
+    _instance.open({id: '3', title: 'Tab 3', panelComponent: (porps) => <p> panel 3 </p>}).then(() => {
+      console.log('tab 3 is open');
+    });
+    // switch to tab 3
+    _instance.select('3').then(() => {
+      console.log('tab 3 is selected');
+    });
+  };
+
+  // end tabs
+
   return (
     <>
       <Routes>
@@ -18,8 +59,15 @@ function App() {
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
+    <>
+      <button onClick={addTab3}>Add tab 3</button>
+      <TabList></TabList>
+      <PanelList></PanelList>
+    </>
     </>
   );
 }
 
 export default App;
+
+
